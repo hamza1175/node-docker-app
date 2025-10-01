fe40cb914b7fe28e30338af27e87293c1c0222eb
hamza1175 <hamzasaleem117588@gmail.com>
2025-10-01 11:34:32 +0500

    added logs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Node.js Docker Application!' });
+  console.log('Root endpoint was accessed');
+  
 });
 
 app.get('/health', (req, res) => {
